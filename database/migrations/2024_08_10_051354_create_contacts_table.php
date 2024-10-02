72474f4e2c3115a3c5c96507ef8790b2f5c67369
Erfan Farokhi <erfanfarokhi84@gmail.com>
2024-10-02 11:20:02 +0330

    change a table name

--- database/migrations/2024_08_10_051354_create_contacts_table.php
+++ database/migrations/2024_08_10_051354_create_contacts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contact', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
             $table->unsignedBigInteger('user_id');
